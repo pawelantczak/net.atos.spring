a9c6dc0e2b7064195b9d7377364c5dc1a0cdbecc
Paweł Antczak <pawel@antczak.org>
2014-09-30 14:50:52 +0200

    Guestbook H2->MySQL update

---
+++ working/04_MySQL/src/main/java/net/atos/spring/guestbook/services/GuestBookService.java
@@ -0,0 +1,15 @@
+package net.atos.spring.guestbook.services;
+
+import net.atos.spring.guestbook.entities.Entry;
+
+import java.util.List;
+
+public interface GuestBookService {
+
+    Entry saveEntry(Entry guestBookEntity);
+
+    List<Entry> findAllEntries();
+
+    List<Entry> findAllEntriesByAuthor(String name);
+
+}
---
+++ working/04_MySQL/src/main/java/net/atos/spring/guestbook/services/GuestBookServiceImpl.java
@@ -0,0 +1,32 @@
+package net.atos.spring.guestbook.services;
+
+import net.atos.spring.guestbook.entities.Entry;
+import net.atos.spring.guestbook.repositories.AuthorDao;
+import net.atos.spring.guestbook.repositories.EntryDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class GuestBookServiceImpl implements GuestBookService {
+
+    @Autowired
+    EntryDao entriesRepository;
+
+    @Autowired
+    AuthorDao authorRepository;
+
+    @Override public Entry saveEntry(Entry guestBookEntity) {
+        return entriesRepository.save(guestBookEntity);
+    }
+
+    @Override public List<Entry> findAllEntries() {
+        return entriesRepository.findAll();
+    }
+
+    @Override public List<Entry> findAllEntriesByAuthor(String name) {
+        return entriesRepository.findAllByAuthor(authorRepository.findByName(name));
+    }
+
+}

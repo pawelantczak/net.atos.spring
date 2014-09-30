e16267839e7ee6ab4841033ceba9815f76348091
Paweł Antczak <pawel@antczak.org>
2014-09-30 14:50:06 +0200

    Guestbook update

--- steps/03_Guestbook/java/net/atos/spring/guestbook/controllers/GuestBookController.java
+++ steps/03_Guestbook/java/net/atos/spring/guestbook/controllers/GuestBookController.java
@@ -1,7 +1,8 @@
-package net.atos.spring.guestbook;
+package net.atos.spring.guestbook.controllers;
 
+import net.atos.spring.guestbook.entities.Entry;
+import net.atos.spring.guestbook.services.GuestBookService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -13,19 +14,19 @@ public class GuestBookController {
     GuestBookService guestBookService;
 
     @RequestMapping(value = "/guestbook/getall", method = RequestMethod.GET)
-    public List<GuestBookEntity> getAll() {
-        return guestBookService.findAll();
+    public List<Entry> getAll() {
+        return guestBookService.findAllEntries();
     }
 
     @RequestMapping(value = "/guestbook/author/{author}", method = RequestMethod.GET)
-    public List<GuestBookEntity> getAllByAuthor(@PathVariable String author) {
-        return guestBookService.findAllByAuthor(author);
+    public List<Entry> getAllByAuthor(@PathVariable String author) {
+        return guestBookService.findAllEntriesByAuthor(author);
     }
 
     @RequestMapping(value = "/guestbook/add", method = RequestMethod.POST)
-    public GuestBookEntity add(@RequestParam String author,
+    public Entry add(@RequestParam String author,
         @RequestParam String content) {
-        return guestBookService.save(new GuestBookEntity(author, content));
+        return guestBookService.saveEntry(new Entry(author, content));
     }
 
 
